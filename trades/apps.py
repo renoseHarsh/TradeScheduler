7bacf4dfac319720e6e276520d10f30fcc134d7d
renoseHarsh <renoseharsh@gmail.com>
2024-12-05 13:33:26 +0530

    Added scheduler

--- trades/apps.py
+++ trades/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class TradesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "trades"
+
+    def ready(self):
+        from .scheduler import start
+
+        start()
